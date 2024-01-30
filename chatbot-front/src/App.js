e7da99f916bad2e87278cea4d885a9bdb35f3914
Filip Lasic <filiplasic22@gmail.com>
2024-01-30 20:29:04 +0100

    Odradjena cela autentifikacija i navbar, kao i prikladan CSS

--- chatbot-front/src/App.js
+++ chatbot-front/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter,Route,Routes, Navigate  } from 'react-router-dom';
+import React, { useState } from 'react';
+import Prijava from './components/prijava/Prijava';
+import Registracija from './components/registracija/Registracija';
+import NavBar from './components/navbar/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+  const [users, setUsers] = useState([]);
+    const [loggedInUser, setLoggedInUser] = useState(null);
+    
+    const handleLogin = (username) => {
+        setLoggedInUser(username);
+        alert(`Uspesno ste se prijavili. Vas username je: ${username}`);
+        console.log(`Uspesna prijava: ${username}`);
+        <p style={{ animation: 'celebration 1s ease-in-out', display: 'inline-block' }}>Welcome, {loggedInUser}! 🎉</p>
+      };
+    
+    const handleRegister = (newUser) => {
+        if (users.some((user) => user.username === newUser.username)) {
+          alert('Ovaj username vec postoji, unesite drugi!');
+          return;
+        }
+    setUsers((prevUsers) => [...prevUsers, newUser]);
+        alert('Uspesna registracija!');
+      };
+
+      const handleLogout = () => {
+        setLoggedInUser(null);
+        return <Navigate to="/" />;
+      };
+      return (
+        <div className="App">
+          <BrowserRouter>
+          {loggedInUser && <NavBar loggedInUser={loggedInUser} handleLogout={handleLogout} />}
+                <Routes>
+                  <Route path="/" element={ loggedInUser ? ( <Navigate to="/pocetna" /> ) 
+                  : (<Prijava onLogin={handleLogin} users={users} /> ) } 
+                  />
+                <Route path="/registracija" element={<Registracija onRegister={handleRegister}
+                       users={users} />} 
+                   />
+                </Routes>
+              
+            </BrowserRouter>
+        </div>
+      );
+    }
 
 export default App;
---
+++ chatbot-front/src/components/navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom';
+import { RiQuestionAnswerFill } from "react-icons/ri";
+import { IoHomeSharp } from "react-icons/io5";
+import { FaHistory } from "react-icons/fa";
+import './Navbar.css';
+import { useNavigate } from 'react-router-dom';
+
+function NavBar({ loggedInUser, handleLogout }) {
+
+    const navigate = useNavigate();
+
+    const handleLogoutClick = () => {
+        handleLogout();
+        navigate('/');
+        };
+
+    return (
+      <div>
+        <nav className="nav">
+          <div className="nav__title">
+            <h1>CHATBOT</h1>
+            <img className='logo'  src="https://i.ibb.co/DrLtmvS/logo.png" alt="logo" border="0"/>
+          </div>
+          <ul className="nav__list">
+            <li className="nav__item">
+                Prijavljen korisnik: {loggedInUser}{' '}
+                </li>
+                <li className="nav__item">
+                  <Link to='/pocetna'> Pocetna <IoHomeSharp /> </Link>
+                </li>
+                <li className="nav__item">
+                  <Link to='/feed'> Pitaj pitanja <RiQuestionAnswerFill /></Link>
+                </li>
+                <li className="nav__item">
+                  <Link to='/korisnici'> Istorija pitanja <FaHistory /></Link>
+                </li>
+                <button className="logout-button" onClick={handleLogoutClick}>
+                Odjava
+              </button>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+  
+  export default NavBar;
