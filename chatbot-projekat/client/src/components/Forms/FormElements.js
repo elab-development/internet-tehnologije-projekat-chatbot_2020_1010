4e33c2a6856b9f8a5d74192d012085f2b525962b
Ilija Jeremić <thejeremic@gmail.com>
2024-08-19 00:35:51 +0200

    Implementirana autentifikacija na klijent strani

---
+++ chatbot-projekat/client/src/components/Forms/FormElements.js
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+
+export const Head = styled.div`
+  font-size: 2rem;
+  text-align: center;
+  color: #7CB9E8;
+`;
+
+export const Select = styled.select`
+  margin-top: 0.3rem;
+  min-width: 25em;
+  background-color: #d3d3d3;
+  height: 2.5rem;
+  padding: 0rem 0.6rem;
+  font-size: 1rem;
+  margin-bottom: 0.2rem;
+  border: none;
+  outline: none;
+  border-radius: 1rem;
+  transition: all 250ms ease-in-out;
+
+  /* Optional: Add a focus style to enhance user experience */
+  &:focus {
+    background-color: #c0c0c0;
+  }
+`;
+
+export const Form = styled.form`
+  margin-top: 1.5rem;
+  display: flex;
+  color: #00c6bd;
+  flex-direction: column;
+`;
+
+export const Formgroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1.938rem;
+  label {
+    font-size: 1rem;
+  }
+`;
+
+export const Input = styled.input`
+  margin-top: 0.3rem;
+  min-width: 25em;
+  background-color: #d3d3d3;
+  height: 2.5rem;
+  padding: 0rem 0.6rem;
+  font-size: 1rem;
+  margin-bottom: 0.2rem;
+  border: none;
+  outline: none;
+  border-radius: 1rem;
+
+  transition: all 250ms ease-in-out;
+`;
+
+export const Para = styled.p`
+  color: #fff7d6;
+  font-size: 0.7rem;
+`;
+export const Linkspan = styled.span`
+  color: #7CB9E8;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+export const Footer = styled.div`
+  color: #ffff;
+  font-size: 0.8rem;
+`;
